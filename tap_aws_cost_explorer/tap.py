54c94e431e8ab339418c711095fdb49852d27d48
gary-beautypie <71257526+gary-beautypie@users.noreply.github.com>
2022-04-19 15:11:52 +0200

    Update tap.py

--- tap_aws_cost_explorer/tap.py
+++ tap_aws_cost_explorer/tap.py
@@ -39,6 +39,7 @@ class TapAWSCostExplorer(Tap):
         th.Property(
             "end_date",
             th.StringType,
+            Optional=True,
             description="The end date for retrieving Amazon Web Services cost."
         ),
         th.Property(
